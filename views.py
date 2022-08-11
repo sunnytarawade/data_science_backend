bd68e644e78e983340132b3274147eed69d07bd4
sunnytarawade <sunnytarawade000@gmail.com>
2022-08-11 15:05:15 +0530

    added folder for handling every upload

--- views.py
+++ views.py
@@ -5,6 +5,7 @@ from werkzeug.utils import secure_filename
 import constants
 import logger
 import time
+import uuid
 
 current_logger = logger.logger
 UPLOAD_FOLDER = constants.UPLOAD_FOLDER
@@ -12,7 +13,8 @@ app_blueprint = Blueprint('app_blueprint',__name__,url_prefix='/')
 
 @app_blueprint.route('/upload', methods=['POST'])
 def fileUpload():
-    target=os.path.join(UPLOAD_FOLDER,'test_docs')
+    upload_id = str(uuid.uuid4())
+    target=os.path.join(UPLOAD_FOLDER,upload_id)
     if not os.path.isdir(target):
         os.mkdir(target)
     current_logger.info("welcome to upload`")
@@ -20,11 +22,14 @@ def fileUpload():
     filename = secure_filename(file.filename)
     destination="/".join([target, filename])
     file.save(destination)
+    cleaned_file_destination = "/".join([target, 'cleaned_file.csv'])
+    file.save(cleaned_file_destination)
     session['uploadFilePath']=destination
-    response="This is response"
+    response={'file_upload_status':'SUCCESS','upload_id' : upload_id}
     return response
 
-@app_blueprint.route('/download/<path:filename>', methods=['GET', 'POST'])
-def download(filename):
-    path = os.path.join(os.getcwd(),constants.UPLOAD_FOLDER,'test_docs',filename)
+@app_blueprint.route('/download/<path:upload_id>', methods=['GET', 'POST'])
+def download(upload_id):
+    path = os.path.join(os.getcwd(),constants.UPLOAD_FOLDER,upload_id,'cleaned_file.csv')
+    print(path)
     return send_file(path, as_attachment=True)
