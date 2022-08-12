cf25a8cfa98dec2e0104a84ea721575706d85d95
sunnytarawade <sunnytarawade000@gmail.com>
2022-08-12 20:05:40 +0530

    added data cleaning for duration, date, station name id integrity

--- constants.py
+++ constants.py
@@ -2,3 +2,5 @@ import os
 
 UPLOAD_FOLDER = os.path.join(os.getcwd(),'upload_files') 
 ALLOWED_EXTENSIONS = set(['csv'])
+
+VALID_DATE_PATTERN = '\d{4}-(?:0[1-9]|1[0-2])-(?:0[1-9]|[1-2]\d|3[0-1])\s(?:[0-1]\d|2[0-3]):[0-5]\d:[0-5]\d\sUTC'
--- views/cleaning.py
+++ views/cleaning.py
@@ -8,6 +8,7 @@ from io import BytesIO
 from zipfile import ZipFile
 
 import pandas as pd
+import re
 
 UPLOAD_FOLDER = constants.UPLOAD_FOLDER
 cleaning_blueprint = Blueprint('cleaning_blueprint',__name__,url_prefix='/clean')
@@ -30,7 +31,7 @@ def download(upload_id):
 @cleaning_blueprint.route('/<path:upload_id>', methods=['GET'])
 def clean_data(upload_id):
 
-    target=os.path.join(UPLOAD_FOLDER,upload_id)
+    target = os.path.join(UPLOAD_FOLDER,upload_id)
 
     # read station_data
     station_data = pd.read_csv(os.path.join(target,'station_data_file_raw.csv'))
@@ -73,11 +74,15 @@ def clean_data(upload_id):
         derived_end_station_id = station_data.loc[station_data['name'] == end_station_name,'id']
         derived_end_station_name = station_data.loc[station_data['id'] == end_station_id,'name']
         
-        if len(derived_end_station_id):
-            station_details['end_station_name'] = station_data.loc[station_data['id'] == derived_end_station_id.values[0],'name'].values[0]
-        elif len(derived_end_station_name):
-            station_details['end_station_id'] = station_data.loc[station_data['name'] == derived_end_station_name.values[0],'id'].values[0]
+        is_end_station_id_valid = bool(len(derived_end_station_name))
+        is_end_station_name_valid = bool(len(derived_end_station_id))
+        
+        if is_end_station_id_valid:
+            station_details['end_station_name'] = station_data.loc[station_data['id'] == end_station_id,'name'].values[0]
+        elif is_end_station_name_valid:
+            station_details['end_station_id'] = station_data.loc[station_data['name'] == end_station_name,'id'].values[0]
         else:
+            station_details['end_station_id'] = 'NA'
             station_details['end_station_name'] = 'NA'
             
         start_station_id = station_details['start_station_id']
@@ -86,19 +91,26 @@ def clean_data(upload_id):
         derived_start_station_id = station_data.loc[station_data['name'] == start_station_name,'id']
         derived_start_station_name = station_data.loc[station_data['id'] == start_station_id,'name']
         
-        if len(derived_start_station_id):
-            station_details['start_station_name'] = station_data.loc[station_data['id'] == derived_start_station_id.values[0],'name'].values[0]
-        elif len(derived_start_station_name):
-            station_details['start_station_id'] = station_data.loc[station_data['name'] == derived_start_station_name.values[0],'id'].values[0]
+        is_start_station_id_valid = bool(len(derived_start_station_name))
+        is_start_station_name_valid = bool(len(derived_start_station_id))
+        
+        if is_start_station_id_valid:
+            station_details['start_station_name'] = station_data.loc[station_data['id'] == start_station_id,'name'].values[0]
+        elif is_start_station_name_valid:
+            station_details['start_station_id'] = station_data.loc[station_data['name'] == start_station_name,'id'].values[0]
         else:
+            station_details['start_station_id'] = 'NA'
             station_details['start_station_name'] = 'NA'
         
-        
         return station_details
-    
+
 
     # applies station_detail_integrity function to every record of rides_data_without_duplicates
     rides_data_without_duplicates = rides_data_without_duplicates.apply(station_detail_integrity, 1)
+
+    # remove values which are equal to 'NA'
+    rides_data_without_duplicates = rides_data_without_duplicates[rides_data_without_duplicates['end_station_name'] != 'NA']
+    rides_data_without_duplicates = rides_data_without_duplicates[rides_data_without_duplicates['start_station_name'] != 'NA']
 
     #  ensures that duration is numeric
     def ensure_duration_is_numeric(ride_details):
@@ -117,9 +129,17 @@ def clean_data(upload_id):
     duration_75_percentile_value =  rides_data_without_duplicates['duration'].describe()['75%']
     rides_data_without_duplicates.loc[rides_data_without_duplicates['duration'] > duration_75_percentile_value,'duration'] = duration_75_percentile_value 
 
-    # remove values which are equal to 'NA'
-    rides_data_without_duplicates = rides_data_without_duplicates[rides_data_without_duplicates['end_station_name'] != 'NA']
-    rides_data_without_duplicates = rides_data_without_duplicates[rides_data_without_duplicates['start_station_name'] != 'NA']
+    # ensures that start_date and end_date match valid date pattern
+    def ensure_data_validity(ride_details):
+        are_dates_valid = bool(re.match(constants.VALID_DATE_PATTERN,ride_details['start_date'])) and bool(re.match(constants.VALID_DATE_PATTERN,ride_details['end_date']))    
+        return ride_details if are_dates_valid else 'NA'
+
+    # applies ensure_data_validity function to all records
+    rides_data_without_duplicates = rides_data_without_duplicates.apply(ensure_data_validity,1)
+
+    # remove records where value of start_date and end_date is 'NA'
+    rides_data_without_duplicates = rides_data_without_duplicates[rides_data_without_duplicates['start_date'] != 'NA']
+    rides_data_without_duplicates = rides_data_without_duplicates[rides_data_without_duplicates['end_date'] != 'NA']
 
     # remove values which are empty
     rides_data_without_duplicates = rides_data_without_duplicates[rides_data_without_duplicates['end_station_name'].notna()]
