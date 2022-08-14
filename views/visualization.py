056c38797cdf4c4265a54fdeaaf7b47e8445c5b5
sunnytarawade <sunnytarawade000@gmail.com>
2022-08-15 00:05:21 +0530

    changed api names

--- views/visualization.py
+++ views/visualization.py
@@ -12,18 +12,18 @@ current_logger = logger.logger
 UPLOAD_FOLDER = constants.UPLOAD_FOLDER
 visualization_blueprint = Blueprint('visualization_blueprint',__name__,url_prefix='/visualize')
 
-@visualization_blueprint.route('/duration_distribution/<path:upload_id>/', methods=['GET'])
-def get_duration_distribution_data(upload_id):
+@visualization_blueprint.route('/duration_distribution/<path:upload_id>/<int:no_of_duration_intervals>/', methods=['GET'])
+def get_duration_distribution_data(upload_id,no_of_duration_intervals):
     data_file_path = os.path.join(UPLOAD_FOLDER,upload_id,'rides_data_file_cleaned.csv')
     rides_data = pd.read_csv(data_file_path)
 
-    duration_ranges_dict = pd.cut(rides_data['duration'], 30).value_counts().sort_index().to_dict()
+    duration_ranges_dict = pd.cut(rides_data['duration'], no_of_duration_intervals).value_counts().sort_index().to_dict()
     duration_ranges = list(map(lambda interval : {'left': interval.left,'right':interval.right,'value':duration_ranges_dict[interval]},list(duration_ranges_dict.keys())))
 
     return duration_ranges
 
-@visualization_blueprint.route('/popular_stations/<path:upload_id>/', methods=['GET'])
-def get_popular_station_map_data(upload_id):
+@visualization_blueprint.route('/popular_stations/<path:upload_id>/<int:no_of_stations>/', methods=['GET'])
+def get_popular_station_map_data(upload_id,no_of_stations):
     rides_data_file_path = os.path.join(UPLOAD_FOLDER,upload_id,'rides_data_file_cleaned.csv')
     rides_data = pd.read_csv(rides_data_file_path)
 
@@ -37,7 +37,7 @@ def get_popular_station_map_data(upload_id):
     station_frequency_dict = {int(k): start_station_freq_dict.get(k, 0) + end_station_freq_dict.get(k, 0) for k in set(start_station_freq_dict) | set(end_station_freq_dict)}
 
     sorted_station_frequency_dict = {k: v for k, v in sorted(station_frequency_dict.items(), key=lambda item: -item[1])}
-    most_popular_stations_ids = list(sorted_station_frequency_dict.keys())[:20]
+    most_popular_stations_ids = list(sorted_station_frequency_dict.keys())[:no_of_stations]
 
     def add_station_access_count(station_details):
             station_details['access_count'] = station_frequency_dict[station_details['id']]
